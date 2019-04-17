2b0993e1aedc61f2056202534c7e8251019eea13
Mohit Kishore <kishoremohit07@gmail.com>
2019-04-17 15:56:21 +0530

    Increased preloader time.

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(2500); // set duration in brackets    
   });
 
 
@@ -22,17 +22,16 @@
 
   /* Back top
   -----------------------------------------------*/
-  
+
   $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
         $('.go-top').fadeIn(200);
         } else {
           $('.go-top').fadeOut(200);
         }
-        });   
+        });
         // Animate the scroll to top
       $('.go-top').click(function(event) {
         event.preventDefault();
       $('html, body').animate({scrollTop: 0}, 300);
       })
-
